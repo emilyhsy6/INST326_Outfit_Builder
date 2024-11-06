73e6bd4006b651b351f1b088673fa29bf42bf7bc
emilyhsy6 <emilyhsy6@gmail.com>
2024-11-06 13:17:32 -0500

    Add files via upload

---
+++ main_outfit_builder.py
@@ -0,0 +1,56 @@
+class ReadTemp:
+    '''Class with different functions to read the current temperature status of the user’s city
+    '''
+    def read_high():
+        ''' Retrieve the highest temperature forecasted for the day 
+        Returns: 
+	        Float: The highest temperature in degrees Fahrenheit
+        '''
+    def read_low(): 
+        ''' Retrieve the lowest temperature forecasted for the day 
+        Return: 
+	        Float: The lowest temperature in degrees Fahrenheit
+        '''
+    def read_average():
+        ''' Calculate the average temperature forecasted for the day 
+        Return: 
+	        Float: The average temperature in degrees Fahrenheit
+        '''
+    def read_humidity():
+        ''' Retrive the humidity level forecasted for the day 
+        Return: 
+	        Float: The humidity level in percentage
+        '''
+    def read_wind_speed():
+        '''Retrive the wind speed forecasted for the day 
+        Return: 
+	        Float: The wind speed in mph
+        '''
+
+
+class Outfit:
+    """Class that determines the various outfit elements based on the weather
+    """
+    def decide_outfit():
+        """Decide the final outfit based on combination of elements
+        """
+    def decide_layers():
+        """Decide how many layers the user should wear
+        """
+    def decide_materials():
+        """Decide what kind of fabric materials the user should wear
+        """
+    def decide_sleeve_or_pant_length():
+        """Decide what length of clothes the user should wear
+        """
+
+def output_weather():
+    """Call the ReadTemp class and display attributes as cohesive statement
+    """
+    
+def output_outfit():
+    """Call the Outfit class and display attributes as cohesive statement
+    """
+def main():
+    """Call the output functions and give user a final response
+    """
