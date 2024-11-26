e4c2d05b4db8eb36e6d4fec1037eedc9946de86f
emilyhsy6 <emilyhsy6@gmail.com>
2024-11-25 20:24:42 -0500

    starting unit test file from template
    write function containing mock data for unit tests

---
+++ test_outfit_builder.py
@@ -0,0 +1,39 @@
+import unittest
+import argparse
+import json
+from main_outfit_builder import get_weather_data, Weather, Outfit, parse_args
+
+class TestOutfitBuilder(unittest.TestCase):
+    def create_test_data(self):
+        self.test_data = {
+            "main": {
+                "temp": 45.0,
+                "temp_max": 47.0,
+                "temp_min": 41.0,
+                "humidity": 80,
+            },
+            "wind": {
+                "speed": 6.0,
+            },
+            "weather": [
+                {"description": "overcast clouds"}
+            ]
+        }
+        self.city = "London"
+        
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
