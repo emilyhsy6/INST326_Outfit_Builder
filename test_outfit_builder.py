a0ec67b451745df6090ad207ce29033cf3ec01fa
SammarJaffery <sammarjaffery04@gmail.com>
2024-11-26 21:25:56 -0500

    added starting point for unit tests

--- test_outfit_builder.py
+++ test_outfit_builder.py
@@ -20,16 +20,17 @@ class TestOutfitBuilder(unittest.TestCase):
         }
         self.city = "London"
         
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_get_weather_data(self):
+        """Assert that output data matches data pulled from API.
+        """
+    
+    def test_Weather(self):
+        """Assert that output is correct and matches data pulled from API.
+        """
+    
+    def test_Outfit(self):
+        """Assert that ouput is correct based on weather data.
+        """
+
 if __name__ == '__main__':
     unittest.main()
